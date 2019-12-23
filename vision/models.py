eae6b98757e0b6c9a9b040ac13e4fde382a52bb5
yanisa <yanisagreen@gmail.com>
2019-12-23 11:38:46 -0500

    add functionality to manage dataset and labels

--- vision/models.py
+++ vision/models.py
@@ -2,13 +2,13 @@ from django.db import models
 # Create your models here.
 class Dataset(models.Model):
     dataset_name = models.CharField(max_length=255)
-    dataset_description = models.CharField(max_length=500)
+    dataset_description = models.CharField(max_length=500, default='')
     def __str__(self):
         return self.dataset_name
 
 class Label(models.Model):
     label_name = models.CharField(max_length=255)
-    label_description = models.CharField(max_length=255)
+    label_description = models.CharField(max_length=255, default='')
     dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE)
     def __str__(self):
         return self.label_name
